90ffc55c6ee1a6cbc1d57cd53a7bb1d37ab0471c
Conner Novicki <connernovicki@gmail.com>
2019-01-25 14:54:57 -0500

    Cleaned up header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,21 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './header.module.scss'
 
-const menuItem = (route) => {
+const menuItem = (route, selectedPage) => {
     return (
-        <li className="menu__item" key={ `menu__item--${route}` }>
+        <li
+            className={`${ route === selectedPage ? `${styles.selected}` : '' }`}
+            key={ `menu__item--${route}` }>
             <Link to={ route }>{ route }</Link>
         </li>
     )
 }
 
-const Header = ({ routes }) => {
+const Header = ({ routes, selectedPage }) => {
     return (
         <header className={styles.header}>
-            <div className={`${styles.container}`}>
-                <nav className="navigation">
-                    <ul className="menu">
-                        { routes.map(route => menuItem(route)) }
+            <div className={styles.container}>
+                <div className={styles.logo}>
+                    LOGO
+                </div>
+                <nav>
+                    <ul className={styles.menu}>
+                        { routes.map(route => menuItem(route, selectedPage)) }
                     </ul>
                 </nav>
             </div>
