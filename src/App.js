dbc97418a9a40f7d977c66e0ab1c1fa20de41e1e
hunter b rose <hunter.b.rose@gmail.com>
2019-02-08 16:30:36 -0500

    add logos and a few styles.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header'
 import Page from './containers/page'
 import { fetchPages } from './actions/wp'
 
+import './styles/app.scss'
+
 class App extends Component {
     componentWillMount() {
         this.props.dispatch(fetchPages())
